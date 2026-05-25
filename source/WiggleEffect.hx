7d8376a9061e526dc646205b677fd61972c88470
andreferrita-collab <andreferrita@gmail.com>
2026-05-25 07:12:09 -0300

    Create WiggleEffect.hx

---
+++ source/WiggleEffect.hx
@@ -0,0 +1,40 @@
+import flixel.FlxCamera;
+import openfl.filters.ShaderFilter;
+import flixel.system.FlxAssets.FlxShader;
+
+class WiggleEffect extends FlxShader
+{
+    @:glFragmentSource('
+    #pragma header
+
+    uniform float uTime;
+    uniform float uSpeed;
+    uniform float uFrequency;
+    uniform float uWaveAmplitude;
+
+    vec2 sineWave(vec2 pt)
+    {
+        float x = 0.0;
+        float y = 0.0;
+
+        float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
+        float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
+
+        pt.y += offsetY;
+        pt.x += offsetX;
+
+        return vec2(pt.x + x, pt.y + y);
+    }
+
+    void main()
+    {
+        vec2 uv = sineWave(openfl_TextureCoordv);
+        gl_FragColor = texture2D(bitmap, uv);
+    }
+    ')
+
+    public function new()
+    {
+        super();
+    }
+}
