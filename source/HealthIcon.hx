9ded27bb83ad15fe9c2a62acfa0fe54f9e5224e3
andreferrita-collab <andreferrita@gmail.com>
2026-05-25 20:42:23 -0300

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,4 +1,16 @@
 package;
 
+import flixel.FlxSprite;
+
 class HealthIcon extends FlxSprite
-{}
+{
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+
+		loadGraphic('assets/images/characters/icons/' + char + '.png');
+
+		antialiasing = true;
+		scrollFactor.set();
+	}
+}
