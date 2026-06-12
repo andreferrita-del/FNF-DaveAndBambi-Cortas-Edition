163aeaeedf65f676fe8ef488cc95d1994ed856a1
andreferrita-collab <andreferrita@gmail.com>
2026-06-12 18:10:46 -0300

    Update WiggleEffect.hx

--- source/WiggleEffect.hx
+++ source/WiggleEffect.hx
@@ -1,9 +1,6 @@
 package;
 
-import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.FlxCamera;
-import openfl.filters.ShaderFilter;
 import flixel.system.FlxAssets.FlxShader;
 
 class WiggleEffect extends ErrorHandledShader
@@ -12,22 +9,19 @@ class WiggleEffect extends ErrorHandledShader
 	#pragma header
 
 	uniform float uTime;
-	uniform float uSpeed = 2;
+	uniform float uSpeed;
 	uniform float uFrequency;
 	uniform float uWaveAmplitude;
 
 	vec2 sineWave(vec2 pt)
 	{
-		float x = 0.0;
-		float y = 0.0;
-
 		float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
 		float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
 
 		pt.y += offsetY;
 		pt.x += offsetX;
 
-		return vec2(pt.x + x, pt.y + y);
+		return pt;
 	}
 
 	void main()
@@ -39,31 +33,23 @@ class WiggleEffect extends ErrorHandledShader
 
 	public function new()
 	{
-		super();
+		super("WiggleEffect"); 
 
-		uTime.value = [0];
-		uSpeed.value = [1];
-		uFrequency.value = [5];
-		uWaveAmplitude.value = [0.01];
-	}
+		try
+		{
+			fx = new WiggleEffect();
 
-	// UPDATE TIMER
-	
-	// ADD WIGGLE EFFECT
-	public static function addWiggleEffect(
-		object:FlxSprite,  // NUMBER 2
-		speed:Float,     // NUMBER 3
-		frequency:Float  // NUMBER 4
-	):WiggleEffect
-	{
-		var wiggle = new WiggleEffect();
+			fx.uSpeed.value = [speed];
+			fx.uFrequency.value = [frequency];
+			fx.uWaveAmplitude.value = [0.1];
 
-		wiggle.uWaveAmplitude.value = [0.1];
-		wiggle.uSpeed.value = [speed];
-		wiggle.uFrequency.value = [frequency];
+			sprite.shader = fx;
+		}
+		catch (e:Dynamic)
+		{
+			ErrorHandler.onShaderError("WiggleEffect", e);
+		}
 
-		object.shader = wiggle;
-
-		return wiggle;
+		return fx;
 	}
 }
