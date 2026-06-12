f16e741bfac5ce15d41b234980b239ddf8c0428d
andreferrita-collab <andreferrita@gmail.com>
2026-06-12 18:57:32 -0300

    Reinitialize ErrorHandler in Main constructor

--- source/Main.hx
+++ source/Main.hx
@@ -9,10 +9,11 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		ErrorHandler.init();
-		
+
 		addChild(new FlxGame(1280, 720, TitleState));
 
 		addChild(new DebugInfo(10, 3));
+		
+ 	    ErrorHandler.init();	
 	}
 }
