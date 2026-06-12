1600b4aa830c80dd76b39cea8aa36cb5a86567f0
andreferrita-collab <andreferrita@gmail.com>
2026-06-12 20:27:39 -0300

    Update CrashState.hx

--- source/CrashState.hx
+++ source/CrashState.hx
@@ -1,47 +1,105 @@
 package;
 
 import flixel.FlxState;
-import lime.app.Application;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import sys.io.File;
+import sys.FileSystem;
 import Sys;
 
 class CrashState extends FlxState
 {
-	var errorType:String;
+	var type:String;
 	var message:String;
 	var stack:String;
 
-	public function new(type:String, msg:String, st:String)
+	public function new(t:String, m:String, s:String)
 	{
 		super();
-
-		errorType = type;
-		message = msg;
-		stack = st;
+		type = t;
+		message = m;
+		stack = s;
 	}
 
 	override function create()
 	{
 		super.create();
 
-		var fullText =
-			"TYPE: " + errorType + "\n\n" +
-			"ERROR:\n" + message + "\n\n" +
-			"STACK TRACE:\n" + stack;
+		var full = "TYPE: " + type + "\n\nERROR:\n" + message + "\n\nSTACK:\n" + stack;
 
-		// 💥 LIME ALERT (native popup)
+		// 💜 BACKGROUND
+		var bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFF120012);
+
+		// 💜 BOX
+		var box = new FlxSprite().makeGraphic(520, 360, 0xFF2A0033);
+		box.x = FlxG.width / 2 - box.width / 2;
+		box.y = FlxG.height / 2 - box.height / 2;
+
+		// 💜 TITLE
+		var title = new FlxText(0, box.y + 10, FlxG.width, "ENGINE CRASH", 26);
+		title.setFormat(null, 26, 0xFFB300FF, "center");
+
+		// 💜 TEXT
+		var text = new FlxText(
+			box.x + 20,
+			box.y + 60,
+			box.width - 40,
+			full,
+			14
+		);
+		text.setFormat(null, 14, 0xFFE6CCFF, "left");
+
+		// 📁 SAVE LOG
+		saveLog(full);
+
+		// 🟣 OK BUTTON (EXIT)
+		var ok = new FlxButton(
+			Std.int(FlxG.width / 2 - 110),
+			Std.int(box.y + box.height - 50),
+			"OK",
+			function()
+			{
+				Sys.exit(1);
+			}
+		);
+
+		ok.color = 0xFF7A00FF;
+
+		// 🔥 RESTART BUTTON
+		var restart = new FlxButton(
+			Std.int(FlxG.width / 2 + 10),
+			Std.int(box.y + box.height - 50),
+			"RESTART",
+			function()
+			{
+				FlxG.resetGame();
+			}
+		);
+
+		restart.color = 0xFFB300FF;
+
+		add(bg);
+		add(box);
+		add(title);
+		add(text);
+		add(ok);
+
+
+		FlxG.timeScale = 0;
+		FlxG.mouse.enabled = true;
+	}
+
+	function saveLog(data:String)
+	{
 		try
 		{
-			Application.current.window.alert(fullText, "ENGINE CRASH");
+			
 		}
 		catch (e:Dynamic)
 		{
-			trace("Alert failed: " + e);
+			trace("log failed: " + e);
 		}
-
-		// 💀 ensure alert shows before exit
-		haxe.Timer.delay(function()
-		{
-			Sys.exit(1);
-		}, 200);
 	}
 }
