f10aeb76298f7c0268aba0e792140428036c151e
andreferrita-collab <andreferrita@gmail.com>
2026-06-13 16:57:49 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,11 +9,12 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		ErrorHandler.init();	
 
 		addChild(new FlxGame(1280, 720, TitleState));
 
-		addChild(new DebugInfo(10, 3));
+		addChild(new FPS(10, 3, 0xFFFFFFFF));
 		
- 	    ErrorHandler.init();	
+
 	}
 }
