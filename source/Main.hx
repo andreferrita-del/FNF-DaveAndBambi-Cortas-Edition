42a1e6ffdd4e941add74a7d76036fd226e699abf
andreferrita-collab <andreferrita@gmail.com>
2026-06-14 20:37:51 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -10,8 +10,12 @@ class Main extends Sprite
 	{
 		super();
 		ErrorHandler.init();	
-
+		
+		#if android
+		addChild(new FlxGame(1280, 720, flixel.mobile.CopyState));
+		#else
 		addChild(new FlxGame(1280, 720, TitleState));
+		#end
 
 		addChild(new FPS(10, 3, 0xFFFFFFFF));
 		
