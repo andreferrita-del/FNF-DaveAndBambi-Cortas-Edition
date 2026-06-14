d3d3c3bae758ff74f079904a26b5522cd64dcd60
andreferrita-collab <andreferrita@gmail.com>
2026-06-14 20:50:22 -0300

    Update CopyState.hx

--- source/flixel/mobile/CopyState.hx
+++ source/flixel/mobile/CopyState.hx
@@ -1,117 +1,83 @@
 package flixel.mobile;
 
-import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.FlxState;
 import flixel.util.FlxTimer;
 
 import openfl.utils.Assets;
-
-import sys.io.File;
-import sys.FileSystem;
-
-import TitleState;
-
-#if android
-import android.Tools;
-import android.Permissions;
-import android.PermissionsList;
 import lime.app.Application;
-#end
+
+import sys.FileSystem;
+import sys.io.File;
 
 class CopyState extends FlxState
 {
-override function create():Void
-{
-super.create();
-
-	var bg = new FlxSprite().makeGraphic(
-		Std.int(FlxG.width),
-		Std.int(FlxG.height),
-		0xFF00AA00
-	);
-	add(bg);
-
-	var txt = new FlxText(
-		0,
-		0,
-		FlxG.width,
-		"Copying game assets..."
-	);
-	txt.setFormat(null, 24, 0xFFFFFFFF, "center");
-	txt.screenCenter();
-	add(txt);
-
-	#if android
-
-	try
+	override function create():Void
 	{
-		Permissions.requestPermissions([
-			PermissionsList.READ_EXTERNAL_STORAGE,
-			PermissionsList.WRITE_EXTERNAL_STORAGE
-		]);
+		super.create();
 
-		var basePath:String =
-			Tools.getExternalStorageDirectory()
-			+ "/Android/data/"
-			+ Application.current.meta.get("packageName")
-			+ "/files/assets/";
+		add(new FlxSprite().makeGraphic(
+			Std.int(FlxG.width),
+			Std.int(FlxG.height),
+			0xFF00AA00
+		));
 
-		if (!FileSystem.exists(basePath))
-			FileSystem.createDirectory(basePath);
+		var txt = new FlxText(0, 0, FlxG.width, "Copying assets...");
+		txt.screenCenter();
+		add(txt);
 
-		for (asset in Assets.list())
+		try
 		{
-			copyAsset(asset, basePath + asset);
+			var files = Assets.list();
+
+			for (file in files)
+			{
+				copyAsset(file);
+			}
+
+			Application.current.window.alert(
+				"All game assets were copied successfully!",
+				"Cortas Edition Setup"
+			);
+		}
+		catch (e:Dynamic)
+		{
+			Application.current.window.alert(
+				Std.string(e),
+				"Copy Error"
+			);
 		}
 
-		Application.current.window.alert(
-			"All game assets have been copied successfully.\nPress OK to continue.",
-			"Cortas Edition Setup"
-		);
-	}
-	catch (e:Dynamic)
-	{
-		Application.current.window.alert(
-			"Failed to copy game assets.\n\n" + Std.string(e),
-			"Cortas Edition Error"
-		);
-	}
-
-	#end
-
-	new FlxTimer().start(1, function(tmr:FlxTimer)
-	{
-		FlxG.switchState(new TitleState());
-	});
-}
-
-function copyAsset(assetPath:String, destination:String):Void
-{
-	try
-	{
-		var slash = destination.lastIndexOf("/");
-
-		if (slash > -1)
+		new FlxTimer().start(1, function(_)
 		{
-			var dir = destination.substr(0, slash);
+			FlxG.switchState(new TitleState());
+		});
+	}
 
-			if (!FileSystem.exists(dir))
+	function copyAsset(asset:String):Void
+	{
+		try
+		{
+			if (!Assets.exists(asset))
+				return;
+
+			var bytes = Assets.getBytes(asset);
+
+			if (bytes == null)
+				return;
+
+			var dir = haxe.io.Path.directory(asset);
+
+			if (dir != "" && !FileSystem.exists(dir))
 				FileSystem.createDirectory(dir);
+
+			File.saveBytes(asset, bytes);
 		}
-
-		var bytes = Assets.getBytes(assetPath);
-
-		if (bytes != null)
+		catch (e:Dynamic)
 		{
-			File.saveBytes(destination, bytes);
+			trace("Failed to copy: " + asset);
 		}
 	}
-	catch (e:Dynamic)
-	{
-		trace(e);
-	}
-}
-
 }
