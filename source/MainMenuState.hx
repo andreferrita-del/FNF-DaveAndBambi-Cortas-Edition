05c86a9abefed1040166aad1b53112263773c8f1
andreferrita-collab <andreferrita@gmail.com>
2026-06-14 18:00:08 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -39,6 +39,7 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
+		throw "TEST ERROR";
 		if (!FlxG.sound.music.playing)
 		{
 			FlxG.sound.playMusic('assets/music/freakyMenu' + TitleState.soundExt);
