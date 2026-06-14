1b271f34aa11cc66203b20afe5dc9dddc28c4b0d
andreferrita-collab <andreferrita@gmail.com>
2026-06-14 16:09:01 -0300

    Refactor WiggleEffect to extend FlxShader

--- source/WiggleEffect.hx
+++ source/WiggleEffect.hx
@@ -1,43 +1,48 @@
 package;
 
-import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxShader;
 
-class WiggleEffect extends ErrorHandledShader
+class WiggleEffect extends FlxShader
 {
-	@:glFragmentSource('
-	#pragma header
+    public var shaderName:String = "WiggleEffect";
 
-	uniform float uTime;
-	uniform float uSpeed;
-	uniform float uFrequency;
-	uniform float uWaveAmplitude;
+    @:glFragmentSource('
+    #pragma header
 
-	vec2 sineWave(vec2 pt)
-	{
-		float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
-		float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
+    uniform floati uTime;
+    uniform float uSpeed;
+    uniform float uFrequency;
+    uniform float uWaveAmplitude;
 
-		pt.y += offsetY;
-		pt.x += offsetX;
+    vec2 sineWave(vec2 pt)
+    {
+        float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
+        float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
 
-		return pt;
-	}
+        return pt + vec2(offsetX, offsetY);
+    }
 
-	void main()
-	{
-		vec2 uv = sineWave(openfl_TextureCoordv);
-		gl_FragColor = texture2D(bitmap, uv);
-	}
-	')
+    void main()
+    {
+        vec2 uv = sineWave(openfl_TextureCoordv);
+        gl_FragColor = texture2D(bitmap, uv);
+    }
+    ')
 
-	public function new()
-	{
-		super("WiggleEffect");
+    public function new()
+    {
+        super();
 
-		//uSpeed.value = [1.0];
-		//uFrequency.value = [5.0];
-		//uWaveAmplitude.value = [0.01];
-	}
+        try
+        {
+           // uTime.value = [0];
+           // uSpeed.value = [1.0];
+           // uFrequency.value = [5.0];
+           // uWaveAmplitude.value = [0.01];
+        }
+        catch (e:Dynamic)
+        {
+            ErrorHandler.onShaderError(shaderName, e);
+        }
+    }
 }
-
