0b3e503f95c48739f8f023ed1b227b85e93a1f4d
andreferrita-collab <andreferrita@gmail.com>
2026-06-14 20:35:37 -0300

    Refactor CopyState for asset transfer and permissions

--- source/flixel/mobile/CopyState.hx
+++ source/flixel/mobile/CopyState.hx
@@ -1,94 +1,117 @@
 package flixel.mobile;
 
 import flixel.FlxState;
-import flixel.text.FlxText;
+import flixel.FlxText;
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.util.FlxTimer;
+
 import openfl.utils.Assets;
+
 import sys.io.File;
 import sys.FileSystem;
+
 import states.TitleState;
-import flixel.FlxG;
 
 #if android
 import android.Tools;
+import android.Permissions;
+import android.PermissionsList;
 import lime.app.Application;
 #end
 
 class CopyState extends FlxState
 {
-override function create()
+override function create():Void
 {
 super.create();
 
-var txt = new FlxText(0, 0, 0, "Initializing asset transfer...");
-add(txt);
+	var bg = new FlxSprite().makeGraphic(
+		Std.int(FlxG.width),
+		Std.int(FlxG.height),
+		0xFF00AA00
+	);
+	add(bg);
 
-#if android
+	var txt = new FlxText(
+		0,
+		0,
+		FlxG.width,
+		"Copying game assets..."
+	);
+	txt.setFormat(null, 24, 0xFFFFFFFF, "center");
+	txt.screenCenter();
+	add(txt);
 
-try
-{
-	var basePath = Tools.getExternalStorageDirectory()
-		+ "/Android/data/"
-		+ Application.current.meta.get("packageName")
-		+ "/files/assets/";
+	#if android
 
-	if (!FileSystem.exists(basePath))
-		FileSystem.createDirectory(basePath);
-
-	var assetList = Assets.list();
-
-	if (assetList.length > 0)
+	try
 	{
-		for (asset in assetList)
+		Permissions.requestPermissions([
+			PermissionsList.READ_EXTERNAL_STORAGE,
+			PermissionsList.WRITE_EXTERNAL_STORAGE
+		]);
+
+		var basePath:String =
+			Tools.getExternalStorageDirectory()
+			+ "/Android/data/"
+			+ Application.current.meta.get("packageName")
+			+ "/files/assets/";
+
+		if (!FileSystem.exists(basePath))
+			FileSystem.createDirectory(basePath);
+
+		for (asset in Assets.list())
 		{
 			copyAsset(asset, basePath + asset);
 		}
 
-		txt.text = "Asset transfer completed successfully.";
+		Application.current.window.alert(
+			"All game assets have been copied successfully.\nPress OK to continue.",
+			"Cortas Edition Setup"
+		);
 	}
-	else
+	catch (e:Dynamic)
 	{
-		txt.text = "No assets to copy.";
+		Application.current.window.alert(
+			"Failed to copy game assets.\n\n" + Std.string(e),
+			"Cortas Edition Error"
+		);
 	}
 
-	FlxG.switchState(new TitleState());
-}
-catch (e:Dynamic)
-{
-	txt.text = "Asset transfer failed.";
-	trace("Error during asset transfer: " + e);
-	FlxG.switchState(new TitleState());
-}
+	#end
 
-#else
-
-txt.text = "This feature is available on Android devices only.";
-FlxG.switchState(new TitleState());
-
-#end
-
-}
-
-function copyAsset(assetPath, destination)
-{
-try
-{
-var dir = destination.substr(0, destination.lastIndexOf("/"));
-
-	if (!FileSystem.exists(dir))
-		FileSystem.createDirectory(dir);
-
-	var bytes = Assets.getBytes(assetPath);
-
-	if (bytes != null)
+	new FlxTimer().start(1, function(tmr:FlxTimer)
 	{
-		File.saveBytes(destination, bytes);
-		trace("Successfully copied asset: " + assetPath);
-	}
-}
-catch (e:Dynamic)
-{
-	trace("Failed to copy asset '" + assetPath + "': " + e);
+		FlxG.switchState(new TitleState());
+	});
 }
 
+function copyAsset(assetPath:String, destination:String):Void
+{
+	try
+	{
+		var slash = destination.lastIndexOf("/");
+
+		if (slash > -1)
+		{
+			var dir = destination.substr(0, slash);
+
+			if (!FileSystem.exists(dir))
+				FileSystem.createDirectory(dir);
+		}
+
+		var bytes = Assets.getBytes(assetPath);
+
+		if (bytes != null)
+		{
+			File.saveBytes(destination, bytes);
+		}
+	}
+	catch (e:Dynamic)
+	{
+		trace(e);
+	}
 }
+
 }
